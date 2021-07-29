0f41bd016a9f14ac0b28bfe6eee2ded2bb7bfaf3
helgs18 <helgs18@student.uia.no>
2021-07-29 23:36:18 +0200

    La til mesademos og ortho2d.c fra pensumbok

---
+++ Computer_Graphics_With_OpenGL/ch02/ortho2d.c
@@ -0,0 +1,35 @@
+#include <GL/glut.h>
+
+void init(void){
+	glClearColor(1.0, 1.0, 1.0, 0.0);	// set display-window color to white
+	
+	glMatrixMode(GL_PROJECTION);		// Set projection parameters
+	gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+void lineSegment(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);	// Clear display
+	
+	glColor3f(0.0, 4.0, 0.2);	// Set line segment color to green
+	glBegin(GL_LINES);
+		glVertex2i(180, 15);	// Specify line segment geometry
+		glVertex2i(10, 145);
+	glEnd();
+
+	glFlush();			// Process all OpenGL routines as quickly as possible
+
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);				// Initialize GLUT
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	// Set display mode
+	glutInitWindowPosition(10, 100);		// Set top-felft display window position
+	glutInitWindowSize(400, 300);			// Set display-window width and height
+	glutCreateWindow("An Example OpenGL Program");	// Create display window
+
+	init();				// Execute initialization procedure
+	glutDisplayFunc(lineSegment);	// Send graphics to display window
+	glutMainLoop();			// Display everything and wait
+}
